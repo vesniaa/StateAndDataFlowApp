8c4cdf97cf182d62bbbd6a8eb4689f3de3b7035e
Eugenia <pyssiki@yandex.ru>
2022-05-20 19:19:02 +0500

    Initial Commit

---
+++ StateAndDataFlowApp/StateAndDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowAppApp.swift
+//  StateAndDataFlowApp
+//
+//  Created by Евгения Аникина on 20.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
